595caa4580165fe31ea2a8cd973ed11bf8accf59
My Name <iam@example.com>
2025-12-04 15:45:33 +0300

    second commit

---
+++ gagaga/script.js
@@ -0,0 +1,52 @@
+// let book1 = "The secret";
+// let book2 = "Думай медленно, решай быстро";
+// let book3 = "Грокаем алгоритмы";
+
+// const maFavBooks = [
+//     "The secret",
+//     "Думай медленно, решай быстро",
+//     "Грокаем алгоритмы",
+// ];
+
+// console.log(book1);
+// console.log(maFavBooks);
+
+// const nums1 = [42];
+// console.log(nums1);
+
+// const nums2 = new Array(42);
+// console.log(nums2)
+
+
+// const nums1 = [42, "is", false];
+
+// console.log(nums1[2])
+
+// nums1[1] = "42 is everithing"
+
+// console.log(nums1);
+
+// nums1[3] = "42"
+
+// console.log(nums1)
+
+
+// const phrases = [
+//     "отправлю другу смешную гифку",
+//     "посмотрю скидки на авиасейлс",
+//     "разберусь, о чем поют реперы",
+//     "расставлю книги на полке по цветам",
+//     "посмотрю на смешных лягушек",
+// ];
+
+// console.log(phrases.length)
+// console.log(phrases[0])
+
+// console.log(phrases[phrases.length - 1])
+
+const saturdayWeekend = ["Saturday"]
+const sundayWeekend = ["Sunday"]
+
+// function getWeekends(long, short) {
+
+// }
