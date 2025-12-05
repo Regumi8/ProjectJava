97623bb9e33490d2128f0d7bb9de1b8f741c76a3
My Name <iam@example.com>
2025-12-05 13:58:16 +0300

    third

--- gagaga/script.js
+++ gagaga/script.js
@@ -31,22 +31,45 @@
 // console.log(nums1)
 
 
-// const phrases = [
-//     "отправлю другу смешную гифку",
-//     "посмотрю скидки на авиасейлс",
-//     "разберусь, о чем поют реперы",
-//     "расставлю книги на полке по цветам",
-//     "посмотрю на смешных лягушек",
-// ];
+const phrases = [
+    "отправлю другу смешную гифку",
+    "посмотрю скидки на авиасейлс",
+    "разберусь, о чем поют реперы",
+    "расставлю книги на полке по цветам",
+    "посмотрю на смешных лягушек",
+    "хочу инжир",
+    "42",
+    "123",
+];
 
-// console.log(phrases.length)
-// console.log(phrases[0])
+// console.log(phrases.length);
+// console.log(phrases[0]);
 
-// console.log(phrases[phrases.length - 1])
+// console.log(phrases[phrases.length - 1]);
 
-const saturdayWeekend = ["Saturday"]
-const sundayWeekend = ["Sunday"]
+// let rand = Math.random() * 10;
+// console.log(rand)
 
-// function getWeekends(long, short) {
+// console.log(Math.floor(9.95));
+// console.log(Math.ceil(9.95));
+// console.log(Math.round(9.5));
+// console.log(Math.max(10, 5, 42));
+// console.log(Math.min(10, 5, 42));
+// console.log(Math.abs(-42));
+// console.log(Math.pow(2, 10));
+// console.log(Math.sqrt(121));
 
-// }
+// console.log(Math.floor(Math.random() * 10))
+
+const randomIndex = Math.floor(Math.random() * phrases.length);
+
+console.log(phrases[randomIndex]);
+
+
+const min = Math.min()
+const max = Math.max()
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+console.log (randomInteger(1, 5))
